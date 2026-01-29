1111096eb67c96610ff31bb6c61ab5cab6c6a6c4
Ryan Nicoletti <r.nicoletti1@gmail.com>
2026-01-28 20:14:59 -0500

    split up main into differnt modules

---
+++ src/article.rs
@@ -0,0 +1,7 @@
+#[derive(Debug, Clone)]
+pub struct Article {
+    pub title: String,
+    pub link: String,
+    pub summary: String,
+    pub date_pub: String,
+}
---
+++ src/scrape/mod.rs
@@ -0,0 +1,2 @@
+pub mod poultryworld;
+pub mod wattagnet;
