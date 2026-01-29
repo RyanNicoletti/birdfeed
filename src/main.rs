b6ed20574d65eae7ca6edce4a0d7d7a5b0bbb302
Ryan Nicoletti <r.nicoletti1@gmail.com>
2026-01-28 20:29:30 -0500

    loop over urls in main

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod rss;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let articles = rss::fetch_from_rss("https://www.cidrap.umn.edu/news/49/rss").await?;
-    println!("articles: {articles:?}");
+    let rss_urls: [&str; 1] = ["https://www.cidrap.umn.edu/news/49/rss"];
+    let mut all_articles: Vec<article::Article> = Vec::new();
+
+    for url in rss_urls {
+        let articles: Vec<article::Article> = rss::fetch_from_rss(url).await?;
+        all_articles.extend(articles);
+    }
+    println!("articles: {all_articles:?}");
     Ok(())
 }
-
-// https://www.cdc.gov/bird-flu/spotlights/index.html
-// https://www.who.int/emergencies/disease-outbreak-news
-// https://www.cidrap.umn.edu/avian-influenza-bird-flu
-// https://news.google.com/rss/search?q=avian+influenza&hl=en-US&gl=US&ceid=US:en
